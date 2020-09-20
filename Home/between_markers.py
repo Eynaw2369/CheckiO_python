c2c584744ea519c345809cd66167296ae5e7beab
Eynaw2369 <k_gnesebok42@hotmail.com>
2020-09-20 20:41:18 +0800

    added new solutions for home

---
+++ Home/between_markers.py
@@ -0,0 +1,34 @@
+def between_markers(text: str, begin: str, end: str) -> str:
+    """
+        returns substring between two given markers
+    """
+    # your code here
+    begin_index = text.find(begin)
+    end_index = text.find(end)
+
+    if begin_index != -1 and end_index != -1:
+        if begin_index < end_index:
+            return text[begin_index + len(begin):end_index]
+        else:
+            return ''
+    elif begin_index != -1 and end_index == -1:
+        return text[begin_index + len(begin):]
+    elif begin_index == -1 and end_index != -1:
+        return text[:end_index]
+    else:
+        return text
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(between_markers('What is >apple<', '>', '<'))
+
+    # These "asserts" are used for self-checking and not for testing
+    assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
+    assert between_markers("<head><title>My new site</title></head>",
+                           "<title>", "</title>") == "My new site", "HTML"
+    assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
+    assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
+    assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
+    assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
+    print('Wow, you are doing pretty good. Time to check it!')
