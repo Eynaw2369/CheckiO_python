74b4ea3676bfb951f7077c8bce0cd512793cdba5
Eynaw2369 <cweitsen@gmail.com>
2021-03-29 16:57:52 +0800

    added new solution to o'reilly

---
+++ O'Reilly/sort_except_zero.py
@@ -0,0 +1,21 @@
+from typing import Iterable
+
+
+def except_zero(items: list) -> Iterable:
+    # your code here
+    y = sorted(i for i in items if i != 0)
+    x = [i if i == 0 else y.pop(0) for i in items]
+    return x
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])) == [1, 3, 0, 0, 4, 4, 5, 0, 7]
+    assert list(except_zero([0, 2, 3, 1, 0, 4, 5])) == [0, 1, 2, 3, 0, 4, 5]
+    assert list(except_zero([0, 0, 0, 1, 0])) == [0, 0, 0, 1, 0]
+    assert list(except_zero([4, 5, 3, 1, 1])) == [1, 1, 3, 4, 5]
+    assert list(except_zero([0, 0])) == [0, 0]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
